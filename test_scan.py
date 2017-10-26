aeb0b0044aebe0d892c0b0e618fe797c9edd1276
Stéphane <salamafet@msn.com>
2017-10-26 19:14:01 +0200

    add scan via socket without thread

--- test_scan.py
+++ test_scan.py
@@ -1,1 +1,21 @@
-import nmap
+import socket, os
+from threading import Thread
+import time
+
+reseau = "192.168.10."
+current_ip = 0
+
+
+
+
+while current_ip < 255:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.3)
+    try:
+        s.connect((reseau + str(current_ip), 80))
+    except socket.error:
+        pass
+    else:
+        print(reseau + str(current_ip) + " ouvert !")
+
+    current_ip += 1
